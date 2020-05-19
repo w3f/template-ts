e370c1e4195416bc062da6db4c8c121f112433fb
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-19 12:49:59 +0200

    initial test

---
+++ test/template.ts
@@ -0,0 +1,41 @@
+import fs from 'fs-extra';
+import { should } from 'chai';
+import path from 'path';
+import tmp from 'tmp';
+
+import { TemplateManager } from '../src/index';
+
+interface Data {
+    key1: string;
+    key2: string;
+}
+
+const subject = new TemplateManager<Data>();
+
+should();
+
+describe('TemplateManager', () => {
+    before(() => {
+        tmp.setGracefulCleanup();
+    });
+
+    describe('create', () => {
+        it('should create a destination file from a template with interpolated data', () => {
+            const tmpobj = tmp.dirSync();
+            const template = 'blabla {{ key1 }} blabla {{ key2 }} bla ';
+            const source = path.join(tmpobj.name, 'source');
+
+            fs.writeFileSync(source, template);
+
+            const target = path.join(tmpobj.name, 'target');
+            const data = { key1: 'value1', key2: 'value2' };
+
+            subject.create(source, target, data);
+
+            const expected = 'blabla value1 blabla value2 bla ';
+            const actual = fs.readFileSync(target).toString();
+
+            actual.should.eq(expected);
+        });
+    });
+});
