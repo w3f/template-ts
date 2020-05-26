da0473a551bd86890b426b6818e41dd70ea8ee23
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-26 12:42:16 +0200

    test for numeric values

--- test/template.ts
+++ test/template.ts
@@ -3,11 +3,28 @@ import { should } from 'chai';
 import path from 'path';
 import tmp from 'tmp';
 
-import { Template } from '../src/index';
+import { Template, TemplateData } from '../src/index';
 
 const subject = new Template();
 
 should();
+
+function check(data: TemplateData, expected: string): void {
+    const tmpobj = tmp.dirSync();
+    const template = 'blabla {{ key1 }} blabla {{ key2 }} bla ';
+    const source = path.join(tmpobj.name, 'source');
+
+    fs.writeFileSync(source, template);
+
+    const target = path.join(tmpobj.name, 'somesubdir', 'target');
+
+    subject.create(source, target, data);
+
+
+    const actual = fs.readFileSync(target).toString();
+
+    actual.should.eq(expected);
+}
 
 describe('TemplateManager', () => {
     before(() => {
@@ -16,21 +33,14 @@ describe('TemplateManager', () => {
 
     describe('create', () => {
         it('should create a destination file from a template with interpolated data', () => {
-            const tmpobj = tmp.dirSync();
-            const template = 'blabla {{ key1 }} blabla {{ key2 }} bla ';
-            const source = path.join(tmpobj.name, 'source');
-
-            fs.writeFileSync(source, template);
-
-            const target = path.join(tmpobj.name, 'somesubdir', 'target');
             const data = { key1: 'value1', key2: 'value2' };
-
-            subject.create(source, target, data);
-
             const expected = 'blabla value1 blabla value2 bla ';
-            const actual = fs.readFileSync(target).toString();
-
-            actual.should.eq(expected);
+            check(data, expected);
+        });
+        it('should allow numeric values', () => {
+            const data = { key1: 'value1', key2: 2 };
+            const expected = 'blabla value1 blabla 2 bla ';
+            check(data, expected);
         });
     });
 });
